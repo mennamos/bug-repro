40ae11a113b2555dc382dd9bee58b2c2be85d1f1
Menna Mostafa <mennatuallah.mustafa@gmail.com>
2022-01-23 13:22:12 +0200

    reproduce gatsby fullstory plugin issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,13 @@
 module.exports = {
-    siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-fullstory",
+      options: {
+        fs_org: process.env.FULLSTORY_ORG,
+      },
     },
-    plugins: [
-
-    ]
-}
+  ],
+};
